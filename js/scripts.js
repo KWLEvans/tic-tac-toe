139338e50e826672a96ad790c8f1433aaff97966
Brynna McCarter and Keith Evans <brynna@>
2017-01-26 15:39:44 -0800

    refactor hard AI for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -156,12 +156,13 @@ var Computer = {
   },
   hard: function() {
     var markedSquares = [];
-    var possibilities = [1, 2, 3];
+
     function computerMark(xIndex, yIndex) {
       if (gameBoard.find(xIndex, yIndex).markedBy() === "") {
         gameBoard.find(xIndex, yIndex).mark(activePlayer);
       }
     }
+
     for (i = 0; i < gameBoard.squares.length; i++) {
       if (gameBoard.squares[i].markedBy()) {
         markedSquares.push(gameBoard.squares[i]);
@@ -169,94 +170,74 @@ var Computer = {
     }
 
     if (activePlayer === playerO) {
-      if (centerSquarePlayer === "O") {
-        diagonalPlay(markedSquares, computerMark);
-      }
+      diagonalPlay(markedSquares, "O", computerMark);
     }
 
     if (activePlayer === playerO) {
-      for (i = 1; i <= 3; i++) {
-        var markedXs = markedSquares.filter(function(square) {
-          return square.xIndex === i;
-        });
-        var markedYs = markedSquares.filter(function(square) {
-          return square.yIndex === i;
-        });
-
-        if (markedXs.length === 2 && (markedXs[0].markedBy() === markedXs[1].markedBy())) {
-          if (markedXs[0].markedBy() === "O") {
-            for (j = 0; j < 3; j++) {
-              if (markedXs[0].yIndex !== possibilities[j] && markedXs[1].yIndex !== possibilities[j]) {
-                console.log("Offensive X (" + i + ", " + possibilities[j] + ")");
-                computerMark(i, possibilities[j]);
-              }
-            }
-          }
-        } else if (markedYs.length === 2 && (markedYs[0].markedBy() === markedYs[1].markedBy())) {
-          if (markedYs[0].markedBy() === "O") {
-            for (j = 0; j < 3; j++) {
-              if (markedYs[0].xIndex !== possibilities[j] && markedYs[1].xIndex !== possibilities[j]) {
-                console.log("offensive Y (" + i + ", " + possibilities[j] + ")");
-                computerMark(possibilities[j], i);
-              }
-            }
-          }
-        }
-      }
+      horizontalVerticalPlay(markedSquares, "O", computerMark);
     }
 
     if (activePlayer === playerO) {
-      if (centerSquarePlayer === "X") {
-        diagonalPlay(markedSquares, computerMark);
-      }
+      diagonalPlay(markedSquares, "X", computerMark);
     }
 
     if (activePlayer === playerO) {
-      for (i = 1; i <= 3; i++) {
-        var markedXs = markedSquares.filter(function(square) {
-          return square.xIndex === i;
-        });
-        var markedYs = markedSquares.filter(function(square) {
-          return square.yIndex === i;
-        });
-
-        if (markedXs.length === 2 && (markedXs[0].markedBy() === markedXs[1].markedBy())) {
-          if (markedXs[0].markedBy() === "X") {
-            for (j = 0; j < 3; j++) {
-              if (markedXs[0].yIndex !== possibilities[j] && markedXs[1].yIndex !== possibilities[j]) {
-                console.log("Defensive X (" + i + ", " + possibilities[j] + ")");
-                computerMark(i, possibilities[j]);
-              }
-            }
-          }
-        } else if (markedYs.length === 2 && (markedYs[0].markedBy() === markedYs[1].markedBy())) {
-          if (markedYs[0].markedBy() === "X") {
-            for (j = 0; j < 3; j++) {
-              if (markedYs[0].xIndex !== possibilities[j] && markedYs[1].xIndex !== possibilities[j]) {
-                console.log("Defensive Y (" + i + ", " + possibilities[j] + ")");
-                computerMark(possibilities[j], i);
-              }
-            }
-          }
-        }
-      }
+      horizontalVerticalPlay(markedSquares, "X", computerMark);
     }
+
     if (activePlayer === playerO) {
       Computer.easy();
     }
   }
 }
 
-function diagonalPlay(markedSquares, computerMark) {
-  if (gameBoard.find(1,1).markedBy() === centerSquarePlayer) {
-    computerMark(3,3);
-  } else if (gameBoard.find(3,3).markedBy() === centerSquarePlayer) {
-    computerMark(1,1);
-  } else if (gameBoard.find(1,3).markedBy() === centerSquarePlayer) {
-    computerMark(3,1);
-  } else if (gameBoard.find(3,1).markedBy() === centerSquarePlayer) {
-    computerMark(1,3);
+function diagonalPlay(markedSquares, playerMark, computerMark) {
+  if (centerSquarePlayer === playerMark) {
+    if (gameBoard.find(1,1).markedBy() === centerSquarePlayer) {
+      computerMark(3,3);
+    } else if (gameBoard.find(3,3).markedBy() === centerSquarePlayer) {
+      computerMark(1,1);
+    } else if (gameBoard.find(1,3).markedBy() === centerSquarePlayer) {
+      computerMark(3,1);
+    } else if (gameBoard.find(3,1).markedBy() === centerSquarePlayer) {
+      computerMark(1,3);
+    }
   } else {
+    if (gameBoard.find(1,1).markedBy() === playerMark && gameBoard.find(3,3).markedBy() === playerMark) {
+      computerMark(2,2);
+    } else if (gameBoard.find(1,3).markedBy() === playerMark && gameBoard.find(3,1).markedBy() === playerMark) {
+      computerMark(2,2);
+    }
+  }
+}
+
+function horizontalVerticalPlay(markedSquares, playerMark, computerMark) {
+  var possibilities = [1, 2, 3];
+
+  for (i = 1; i <= 3; i++) {
+    var markedXs = markedSquares.filter(function(square) {
+      return square.xIndex === i;
+    });
+    var markedYs = markedSquares.filter(function(square) {
+      return square.yIndex === i;
+    });
+    if (markedXs.length === 2 && (markedXs[0].markedBy() === markedXs[1].markedBy())) {
+      if (markedXs[0].markedBy() === playerMark) {
+        for (j = 0; j < 3; j++) {
+          if (markedXs[0].yIndex !== possibilities[j] && markedXs[1].yIndex !== possibilities[j]) {
+            computerMark(i, possibilities[j]);
+          }
+        }
+      }
+    } else if (markedYs.length === 2 && (markedYs[0].markedBy() === markedYs[1].markedBy())) {
+      if (markedYs[0].markedBy() === playerMark) {
+        for (j = 0; j < 3; j++) {
+          if (markedYs[0].xIndex !== possibilities[j] && markedYs[1].xIndex !== possibilities[j]) {
+            computerMark(possibilities[j], i);
+          }
+        }
+      }
+    }
   }
 }
 
