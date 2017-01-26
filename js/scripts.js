70b63a815539d1eff7dd629d010bf4306499c766
Brynna McCarter and Keith Evans <keith@>
2017-01-25 16:50:04 -0800

    finish object structure, except for game.over

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 ///////////////////////////////
 //Back-End
 //////////////////////////////
-var mark = "X";
+var playerX;
+var playerO;
+var gameBoard;
+var activePlayer;
 
 function Player(mark) {
   this.mark = mark;
@@ -13,11 +16,13 @@ function Square(xIndex, yIndex) {
 }
 
 Square.prototype.mark = function(player) {
-  $("#board").nth-child(this.yIndex).nth-child(this.xIndex).text(player.mark);
+  $("#board").children().eq(this.yIndex - 1).children().eq(this.xIndex - 1).text(player.mark);
+  changeActivePlayer();
+  // Game.over();
 }
 
 Square.prototype.markedBy = function() {
-  var mark = $("#board").nth-child(this.yIndex).nth-child(this.xIndex).text();
+  var mark = $("#board").children().eq(this.yIndex - 1).children().eq(this.xIndex - 1).text();
   return mark;
 }
 
@@ -33,26 +38,79 @@ Board.prototype.build = function() {
   }
 }
 
-function changeMark() {
-  if (mark === "X") {
-    mark = "O";
+Board.prototype.find = function(xIndex, yIndex) {
+  var foundSquare;
+  this.squares.forEach(function(square) {
+    if (square.xIndex === xIndex && square.yIndex === yIndex) {
+      foundSquare = square;
+    }
+  });
+  return foundSquare;
+}
+
+var Game = {
+  start: function() {
+    playerX = new Player("X");
+    activePlayer = playerX;
+    playerO = new Player("O");
+    gameBoard = new Board();
+    gameBoard.build();
+  },
+  // over: function() {
+  //   var xX = ["X"];
+  //   var xY = ["X"];
+  //   var oX = ["O"];
+  //   var oY = ["O"];
+  //   var allArrays = [];
+  //   var winner;
+  //
+  //   for (i = 1; i < gameBoard.squares.length; i++) {
+  //     if (gameBoard.squares[i].markedBy() === "X") {
+  //       xX.push(gameBoard.squares[i].xIndex);
+  //     } else if (gameBoard.squares[i].markedBy() === "O") {
+  //       oX.push(gameBoard.squares[i].xIndex);
+  //     }
+  //     if (gameBoard.squares[i].markedBy() === "X") {
+  //       xY.push(gameBoard.squares[i].yIndex);
+  //     } else if (gameBoard.squares[i].markedBy() === "O") {
+  //       oY.push(gameBoard.squares[i].yIndex);
+  //     }
+  //   }
+  //
+  //   allArrays.push(xX, xY, oX, oY);
+  //
+  //   for (i = 0; i < allArrays.length; i++) {
+  //     if (allArrays[i].length === 4) {
+  //       console.log(allArrays);
+  //       console.log(allArrays[i]);
+  //       winner = allArrays[i][0];
+  //       alert("The winner is " + winner + "!");
+  //     }
+  //   }
+  // }
+}
+
+function changeActivePlayer() {
+  if (activePlayer.mark === "X") {
+    activePlayer = playerO;
   } else {
-    mark = "X";
+    activePlayer = playerX;
   }
 }
+
+
+
 ///////////////////////////////
 //Front-End
 //////////////////////////////
 
-
 $(function() {
+  Game.start();
+
   $(".row div").click(function() {
-    $(this).text(mark);
     $(this).unbind("click");
     var xIndex = 3 - $(this).nextAll().length;
     var yIndex = 3 - $(this).parent().nextAll().length;
-    console.log("X index: " + xIndex);
-    console.log("Y index: " + yIndex);
-    changeMark();
+    gameBoard.find(xIndex, yIndex).mark(activePlayer);
   });
 });
